1413f43e33f85a0f21f65dbad083d87d8faf4670
maida-fl <maida.guzman@outlook.com>
2023-04-18 10:47:31 -0300

    search functionality

--- src/api.js
+++ src/api.js
@@ -41,3 +41,6 @@ export const gameDetailsURL = (game_id) =>
 //Game Screenshots
 export const gameScreenshotURL = (game_id) =>
   `${base_url}games/${game_id}/screenshots?key=${process.env.REACT_APP_GAMES_API}`;
+//Searched game
+export const searchGameURL = (game_name) =>
+  `${base_url}games?key=${process.env.REACT_APP_GAMES_API}&search=${game_name}&page_size=9`;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,13 +21,34 @@ const Home = () => {
     dispatch(loadGames());
   }, [dispatch]);
   // Get data back
-  const { popular, newGames, upcoming } = useSelector((state) => state.games);
+  const { popular, newGames, upcoming, searched } = useSelector(
+    (state) => state.games
+  );
   return (
     <GameList>
       <motion.div type="crossfade" layout={pathId}>
         <AnimatePresence>
           {pathId && <GameDetail pathId={pathId} />}
         </AnimatePresence>
+        {searched.length ? (
+          <div className="searched">
+            <h2>Searched Games</h2>
+            <Games>
+              {searched &&
+                searched.map((game) => (
+                  <Game
+                    name={game.name}
+                    released={game.released}
+                    id={game.id}
+                    image={game.background_image}
+                    key={game.id}
+                  />
+                ))}
+            </Games>
+          </div>
+        ) : (
+          ""
+        )}
         <h2>Upcoming Games</h2>
         <Games>
           {upcoming &&
